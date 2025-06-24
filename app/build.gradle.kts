265f25cb27e2648bab9b000f5ff9d23f4979b5ae
JhosephvA <henry.ventura@tecsup.edu.pe>
2025-06-23 23:14:26 -0500

    Configuramos mas build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,20 +27,27 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
+
     buildFeatures {
         compose = true
     }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5"
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -49,6 +56,16 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    // For AppWidgets support
+    implementation ("androidx.glance:glance-appwidget:1.1.0")
+
+// For interop APIs with Material 3
+    implementation ("androidx.glance:glance-material3:1.1.0")
+
+// For interop APIs with Material 2
+    implementation ("androidx.glance:glance-material:1.1.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -56,15 +73,4 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-
-    // Soporte para widgets AppWidget
-    implementation("androidx.glance:glance-appwidget:1.1.0")
-
-// Interoperabilidad con Material 3
-    implementation("androidx.glance:glance-material3:1.1.0")
-
-// Interoperabilidad con Material 2
-    implementation("androidx.glance:glance-material:1.1.0")
-
-
 }
