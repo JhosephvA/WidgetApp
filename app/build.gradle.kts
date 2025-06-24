0978ce65b83d17d18ee7f1180116f0c8761f0d57
JhosephvA <henry.ventura@tecsup.edu.pe>
2025-06-23 23:09:33 -0500

    Creamos y agregamos dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,15 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Soporte para widgets AppWidget
+    implementation("androidx.glance:glance-appwidget:1.1.0")
+
+// Interoperabilidad con Material 3
+    implementation("androidx.glance:glance-material3:1.1.0")
+
+// Interoperabilidad con Material 2
+    implementation("androidx.glance:glance-material:1.1.0")
+
+
 }
